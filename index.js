c1217ef125b7166a2b865afcc7cefe1279c8be46
Arshal Daison Mathew <arshaldmathew@gmail.com>
2023-04-05 18:19:41 +0530

    Changed port

--- index.js
+++ index.js
@@ -54,6 +54,6 @@ app.get('/runMergeFn', (req, res) => {
     res.status(200).send(JSON.stringify(arr));
 
 })
-app.listen(3000,()=>{
+app.listen(process.env.PORT||3000,()=>{
     console.log('listening on 3000')
 })
