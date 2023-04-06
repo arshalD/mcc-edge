a4755ccf3f6ae8f95d52f51afe3b70e12b59142c
Arshal Daison Mathew <arshaldmathew@gmail.com>
2023-04-06 22:39:06 +0530

    Changed sort

--- index.js
+++ index.js
@@ -40,7 +40,28 @@ function merge_Arrays(left_sub_array, right_sub_array) {
     const left_sub_array = unsorted_Array.splice(0, middle_index)
     return merge_Arrays(mergeSort(left_sub_array),mergeSort(unsorted_Array))
  }
+  
+ function selectionSort(arr,  n)
+ {
+     var i, j, min_idx;
+  
+     // One by one move boundary of unsorted subarray
+     for (i = 0; i < n-1; i++)
+     {
+         // Find the minimum element in unsorted array
+         min_idx = i;
+         for (j = i + 1; j < n; j++)
+         if (arr[j] < arr[min_idx])
+             min_idx = j;
 
+             let temp = arr[min_idx];
+             arr[min_idx] = arr[i];
+             arr[i] = temp;
+         // Swap the found minimum element with the first element
+         // swap(arr,min_idx, i);
+     }
+     return arr;
+ }
 
 app.get('/runMergeFn', (req, res) => {
     let startTime = Date.now()
@@ -48,7 +69,7 @@ app.get('/runMergeFn', (req, res) => {
     const array = req.body.array
     console.log(typeof(array))
     var arr = array;
-	arr=mergeSort(arr);
+	arr=selectionSort(arr, array.length);
     let time = Date.now() - startTime
     console.log("Time taken: ", time,"ms")
     res.status(200).send(JSON.stringify(arr));
